51d5658d789ead3435c07087b2f4e5f00c6ad5a6
Touhidur Rahman <touhidur.rahman@check24.de>
2021-09-24 23:38:28 +0200

    Wip stripe

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\SubsubcategoryController;
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Admin\ColorController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StockController;
 use App\Http\Livewire\Auth\Login;
@@ -31,6 +32,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/shop', [HomeController::class, 'shop'])->name('shop');
 Route::get('/product/{product}', [HomeController::class, 'productDetails'])->name('productDetails');
+
+Route::get('/checkout', [CheckoutController::class, 'show'])->name('checkout.show');
+Route::post('/checkout', [CheckoutController::class, 'pay'])->name('checkout.pay');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
