9f2b009dd81a29b44c2c5e03d22aba17e087a039
Touhidur Rahman <touhidur.rahman@check24.de>
2021-09-26 21:58:18 +0200

    Fix Assets force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,12 +22,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         // https://laracasts.com/discuss/channels/laravel/mixed-content-issue-content-must-be-served-as-https
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
